48d0923e8b23085c7e5a9824db253275f114ac7c
Andres Aguilar <andres@assembla.com>
2017-02-03 13:14:21 -0600

    Implement 'Show more' feature instead of a pagination

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def per_page
-    Rails.configuration.settings['max_results']
+    Rails.configuration.settings['per_page']
   end
 
   def cache_expire
@@ -10,6 +10,6 @@ class ApplicationController < ActionController::Base
   end
 
   def race_condition_ttl
-    Rails.configuration.settings['max_results'].minutes
+    Rails.configuration.settings['cache_race_ttl'].minutes
   end
 end
--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -6,19 +6,22 @@ class PhotosController < ApplicationController
   def search
     redirect_to root_path and return if params[:query].blank?
     @photos = []
+    @query = params[:query]
+    @page = params[:page] || 1
     FlickRaw.api_key = Rails.application.secrets.flickr_key
     FlickRaw.shared_secret = Rails.application.secrets.flickr_secret
 
-    @photos = Rails.cache.fetch(params[:query],
+    @photos = Rails.cache.fetch("#{params[:query]} page:#{@page}",
       expires_in: cache_expire,
       race_condition_ttl: race_condition_ttl) do
 
-      flickr.photos.search(tags: params[:query], per_page: per_page ).each do |photo|
-        @photos << photo
-      end
+      flickr.photos.search(tags: params[:query], per_page: per_page, page: @page.to_i)
+    end
+    respond_to do |format|
+      format.html { render :index }
+      format.js { render :photos}
     end
 
-    render :index
   end
 
 end
